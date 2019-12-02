7eae420bb937b2c351202640bf89432863a12b56
Jakob Rehmann <jakob.rehmann@gmail.com>
2019-12-02 14:58:32 +0100

    will the build work?

--- src/main/java/org/matsim/effectiveJava/Item7.java
+++ src/main/java/org/matsim/effectiveJava/Item7.java
@@ -1,29 +1,29 @@
-package org.matsim.effectiveJava;
-
-public class Item7 {
-
-    public static void main(String[] args) {
-
-        Stack stack = new Stack();
-        System.out.println("Start Pushing");
-
-        for (int i = 0; i <= 5; i++) {
-            String str = "Object " + i;
-            System.out.println("Push " + str);
-            stack.push(str);
-        }
-
-        System.out.println("\nStart Popping");
-
-        for (int i = 0; i < 3; i++) {
-            Object obj = stack.pop();
-            System.out.println("Pop " + obj);
-        }
-
-        System.out.println("Finished");
-
-        System.out.println(stack.getSize());
-
-
-    }
-}
+//package org.matsim.effectiveJava;
+//
+//public class Item7 {
+//
+//    public static void main(String[] args) {
+//
+//        Stack stack = new Stack();
+//        System.out.println("Start Pushing");
+//
+//        for (int i = 0; i <= 5; i++) {
+//            String str = "Object " + i;
+//            System.out.println("Push " + str);
+//            stack.push(str);
+//        }
+//
+//        System.out.println("\nStart Popping");
+//
+//        for (int i = 0; i < 3; i++) {
+//            Object obj = stack.pop();
+//            System.out.println("Pop " + obj);
+//        }
+//
+//        System.out.println("Finished");
+//
+//        System.out.println(stack.getSize());
+//
+//
+//    }
+//}

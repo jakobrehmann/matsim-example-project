a4d376ef283e6f4645d113ad34f5a5a2e144f270
Jakob Rehmann <jakob.rehmann@gmail.com>
2019-12-02 15:11:05 +0100

    will the build work? 3 ???

--- src/main/java/org/matsim/advMATSim/GuiceWithoutMatsim.java
+++ src/main/java/org/matsim/advMATSim/GuiceWithoutMatsim.java
@@ -3,13 +3,20 @@ package org.matsim.advMATSim;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.Module;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+
+import static org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists;
 
 public class GuiceWithoutMatsim {
 
     public static void main(String[] args) {
 
-        Module module = new AbstractModule() { // from GOOGLE!!!!!!
+        com.google.inject.Module module = new AbstractModule() { // from GOOGLE!!!!!!
             @Override
             protected void configure() {
                 bind(SomeInterface.class ).to(MyImplementation.class);//interface to inplementation
